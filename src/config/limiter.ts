c39ce2918b85b77e92f65e3410cc061a7f1562c2
samuel <samusplay08@gmail.com>
2026-06-02 20:43:54 -0500

    limiter con variables de entorno

--- src/config/limiter.ts
+++ src/config/limiter.ts
@@ -3,6 +3,7 @@ import { rateLimit } from 'express-rate-limit'
 export const limiter=rateLimit({
     //limita request
     windowMs:60*1000,
+    //limite de pruebas para cambiar ambientes
     limit:process.env.NODE_ENV==='production'?5:100,
     message:{"error":"Has alcanzado el limite de peticiones"}
 })
