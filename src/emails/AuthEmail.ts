28e8c2398e98f40eee5dd9c926e0fd869d0ba7ce
samuel <samusplay08@gmail.com>
2026-07-14 11:58:01 -0500

    cambios correo url

--- src/emails/AuthEmail.ts
+++ src/emails/AuthEmail.ts
@@ -15,7 +15,7 @@ export class AuthEmail{
             html:`
             <p>Hola:${user.name} Has creado tu cuenta en Cashtrackr,ya esta casi lista </p>
             <p>Visita el siguiente enlace:</p>
-            <a href="#">Confirmar cuenta </a>
+            <a href="${process.env.FRONTEND_URL}/auth/confirm-account">Confirmar cuenta </a>
             <p>e ingresa el codigo:<b>${user.token}</b></p>`
                         
         })
@@ -32,7 +32,7 @@ export class AuthEmail{
             html:`
             <p>Hola:${user.name} Has solicitado reestablecer tu password </p>
             <p>Visita el siguiente enlace:</p>
-            <a href="#">Reestablecer Password</a>
+            <a href="${process.env.FRONTEND_URL}/auth/new-password">Reestablecer Password</a>
             <p>e ingresa el codigo:<b>${user.token}</b></p>`
                         
         })
